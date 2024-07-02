6b6ea078a5c67bea552759fc7c94950608a030cc
João Gabriel Torreiro Gila <joaogabrieltg@gmail.com>
2024-07-02 19:57:28 -0300

    impl busca por logradouro

--- lib/modules/consulta_cep/presentation/page/busca_cep_page.dart
+++ lib/modules/consulta_cep/presentation/page/busca_cep_page.dart
@@ -1,6 +1,8 @@
 import 'package:estudo_flutter/modules/consulta_cep/presentation/stores/busca_cep_store.dart';
 import 'package:estudo_flutter/shared/mobx/loading_store.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:estudo_flutter/shared/widgets/global_themes.dart';
@@ -11,14 +13,19 @@ class BuscaCepPage extends StatefulWidget {
   @override
   _BuscaCepPageState createState() => _BuscaCepPageState();
 }
+
 final GlobalWidgets widgets = GlobalWidgets();
 final ThemeColors themeColors = ThemeColors();
 
 class _BuscaCepPageState extends State<BuscaCepPage> {
   final loadingStore = LoadingStore();
   final TextEditingController _textController = TextEditingController();
+  final TextEditingController _textController2 = TextEditingController();
   String _displayText = '';
   final BuscaCepStore _buscaCepStore = Modular.get<BuscaCepStore>();
+  String? estadoSelecionado;
+  String? cidadeSelecionada;
+  List<String> cidades = [];
 
   Future<void> _confirmText() async {
     loadingStore.isLoading = true;
@@ -59,59 +66,9 @@ class _BuscaCepPageState extends State<BuscaCepPage> {
                 style: widgets.titleStyle,
               ),
               const SizedBox(height: 86),
-              SizedBox(
-                width: 302,
-                child: TextFormField(
-                  focusNode: FocusNode(),
-                  autofocus: true,
-                  controller: _textController,
-                  style: widgets.textInputStyle,
-                  textInputAction: TextInputAction.done,
-                  decoration: InputDecoration(
-                    hintText: "Digite o CEP para a busca",
-                    hintStyle: widgets.textInputStyle,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        22,
-                      ),
-                      borderSide: BorderSide(
-                        color: themeColors.greyWhite,
-                        width: 1,
-                      ),
-                    ),
-                    focusedBorder: widgets.outlineBorder,
-                    disabledBorder: widgets.outlineBorder,
-                    border: widgets.outlineBorder,
-                    filled: true,
-                    //fillColor: themeColors.greyWhite,
-                    fillColor: const Color(0XFFF6F6F6),
-                    isDense: true,
-                    contentPadding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 12,
-                    ),
-                  ),
-                ),
-              ),
+              buildCepInput(_textController, "Digite o CEP para a busca"),
               const SizedBox(height: 10),
-              Container(
-                width: double.maxFinite,
-                height: 32,
-                margin: const EdgeInsets.only(
-                  left: 30,
-                  right: 28,
-                ),
-                child: ElevatedButton(
-                  style: widgets.elevatedButtonStyle,
-                  onPressed: () {
-                    _confirmText();
-                  },
-                  child: Text(
-                    "Buscar",
-                    style: widgets.buttonTextStyle,
-                  ),
-                ),
-              ),
+              confirmButton(true),
               const SizedBox(height: 40),
               Observer(builder: (_) {
                 LoadingStore loadingStore = LoadingStore();
@@ -126,10 +83,141 @@ class _BuscaCepPageState extends State<BuscaCepPage> {
                   );
                 }
               }),
+              Container(
+                child: buildEstadoCidadeSelecao(),
+              ),
+              Container(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildCepInput(TextEditingController controller, String hintText) {
+    return SizedBox(
+        width: 302,
+        child: TextFormField(
+          focusNode: FocusNode(),
+          autofocus: true,
+          controller: controller,
+          style: widgets.textInputStyle,
+          textInputAction: TextInputAction.done,
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: widgets.textInputStyle,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(
+                22,
+              ),
+              borderSide: BorderSide(
+                color: themeColors.greyWhite,
+                width: 1,
+              ),
+            ),
+            focusedBorder: widgets.outlineBorder,
+            disabledBorder: widgets.outlineBorder,
+            border: widgets.outlineBorder,
+            filled: true,
+            //fillColor: themeColors.greyWhite,
+            fillColor: const Color(0XFFF6F6F6),
+            isDense: true,
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 12,
+            ),
+          ),
+        ));
+  }
+
+  Widget confirmButton(bool buscaPorCep) {
+    return Container(
+      width: double.maxFinite,
+      height: 32,
+      margin: const EdgeInsets.only(
+        left: 30,
+        right: 28,
+      ),
+      child: ElevatedButton(
+        style: widgets.elevatedButtonStyle,
+        onPressed: () {
+          if (buscaPorCep) {
+            _confirmText();
+          } else {
+            // Implementar ação após seleção da cidade
+            print('$estadoSelecionado/$cidadeSelecionada/${_textController2.text}');
+          }
+        },
+        child: Text(
+          "Buscar",
+          style: widgets.buttonTextStyle,
+        ),
+      ),
+    );
+  }
+
+  Widget buildEstadoCidadeSelecao() {
+    return FutureBuilder<List<Estado>>(
+      future: _buscaCepStore.carregarEstadosCidades(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (snapshot.hasData) {
+          List<Estado> estados = snapshot.data!;
+          return Column(
+            children: [
+              DropdownButton<String>(
+                hint: const Text("Selecione o Estado"),
+                value: estadoSelecionado,
+                onChanged: (newValue) {
+                  setState(() {
+                    estadoSelecionado = newValue;
+                    cidades = estados
+                        .firstWhere((estado) => estado.sigla == newValue)
+                        .cidades;
+                  });
+                },
+                items: estados.map<DropdownMenuItem<String>>((Estado estado) {
+                  return DropdownMenuItem<String>(
+                    value: estado.sigla,
+                    child: Text(estado.nome),
+                  );
+                }).toList(),
+              ),
+              DropdownButton<String>(
+                hint: const Text("Selecione a Cidade"),
+                value: cidades.isNotEmpty ? cidadeSelecionada : null,
+                onChanged: cidades.isNotEmpty
+                    ? (newValue) {
+                        setState(() {
+                          cidadeSelecionada = newValue;
+                        });
+                        // Implementar ação após seleção da cidade
+                        print('$estadoSelecionado/$cidadeSelecionada/');
+                      }
+                    : null,
+                items: cidades.map<DropdownMenuItem<String>>((String cidade) {
+                  return DropdownMenuItem<String>(
+                    value: cidade,
+                    child: Text(cidade),
+                  );
+                }).toList(),
+              ),
+              cidadeSelecionada != null
+                  ? Column(children: [
+                      buildCepInput(_textController2, "Digite o logradouro para a busca"),
+                      const SizedBox(height: 10),
+                      confirmButton(false),
+                    ])
+                  : Container(),
+            ],
+          );
+        } else {
+          return const Text('No data found');
+        }
+      },
     );
   }
 }
--- lib/modules/consulta_cep/presentation/stores/busca_cep_store.dart
+++ lib/modules/consulta_cep/presentation/stores/busca_cep_store.dart
@@ -1,5 +1,7 @@
 import '../../domain/user_case/busca_cep_case.dart';
 import '../../data/model/cep_model.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 //todo implementar o store (use_case -> stores -> pages)
 
 class BuscaCepStore {
@@ -17,5 +19,29 @@ class BuscaCepStore {
     };
     _buscaCepCase.addToDatabase(cepData);
     return 'Logradouro: ${cepModel.logradouro}\nBairro: ${cepModel.bairro}\nLocalidade: ${cepModel.localidade}\nUF: ${cepModel.uf}\nDDD: ${cepModel.ddd}';
+  }
+
+  Future<List<Estado>> carregarEstadosCidades() async {
+    String response = await rootBundle.loadString('lib/shared/assets/data/estados_cidades.json');
+    var data = json.decode(response);
+    List<Estado> estados =
+        (data['estados'] as List).map((i) => Estado.fromJson(i)).toList();
+    return estados;
+  }
+}
+
+class Estado {
+  final String sigla;
+  final String nome;
+  final List<String> cidades;
+
+  Estado({required this.sigla, required this.nome, required this.cidades});
+
+  factory Estado.fromJson(Map<String, dynamic> json) {
+    return Estado(
+      sigla: json['sigla'],
+      nome: json['nome'],
+      cidades: List<String>.from(json['cidades']),
+    );
   }
 }
